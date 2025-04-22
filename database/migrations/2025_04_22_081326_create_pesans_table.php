b800c5901fe7d70bef91f277af272ac682300347
NAUFAL AUSHAF <nopal9314@gmail.com>
2025-04-22 20:12:17 +0700

    Migration

---
+++ database/migrations/2025_04_22_081326_create_pesans_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pesans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pelanggan_id')->constrained('pelanggans')->onDelete('cascade');
+            $table->foreignId('makanan_id')->nullable()->constrained('makanans')->onDelete('set null');
+            $table->foreignId('minuman_id')->nullable()->constrained('minumans')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pesans');
+    }
+};
